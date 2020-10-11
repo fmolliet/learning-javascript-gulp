71d435cf0951f2ad32aaa567b319672773498e04
fmolliet <fmolliet@hotmail.com>
2020-10-11 03:07:54 -0300

    Adicionando compilação do SASS + correção no compiler + refatoração geral

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,62 @@
-const { series, parallel, src, dest } = require('gulp');
+const { series, parallel, src, dest, watch } = require('gulp');
+// Dependencias para SASS
+const sass         = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const csso         = require('gulp-csso');
+sass.compiler      = require('node-sass');
+sass.sourcemaps    = require('gulp-sourcemaps');
+
 // Dependencias Para TS
 const { createProject } = require('gulp-typescript'); // https://www.npmjs.com/package/gulp-typescript
-const tsProject = createProject('tsconfig.json'); // npx tsc --init 
+const tsProject         = createProject('tsconfig.json'); // npx tsc --init 
 // Dependencias Para ES6 
-const babel = require('gulp-babel'); // https://github.com/babel/gulp-babel
+const babel  = require('gulp-babel'); // https://github.com/babel/gulp-babel
 const uglify = require('gulp-uglify');
 
-
-function clean(cb) {
-  // body omitted
-  cb();
-}
-
-function cssTranspile(cb) {
-  // body omitted
-  cb();
-}
-
-function cssMinify(cb) {
-  // body omitted
-  cb();
-}
-
-function jsTranspile(cb) {
-  // body omitted
-  cb();
-}
-
-function jsBundle(cb) {
-  // body omitted
-  cb();
-}
-
-function jsMinify(cb) {
-  // body omitted
-  cb();
-}
-// TODO: refatorar 
+// TODO: montar um cleaner 
+// Multi propose compiler   ( https://gulpjs.com/docs/en/getting-started/creating-tasks )
 exports.compile = series(
-  clean,
   parallel(
-    cssTranspile,
-    series(jsTranspile, jsBundle)
+    sassCompile,
+    series(typescriptTranspile, babelTranspile)
   ),
-  parallel(cssMinify, jsMinify)
 );
 
-// Compilação para type
-exports.typescript = function() {
-  var tsResult = src("src/**/*.ts") 
+// compilação de Sass //
+function sassCompile(){
+  return src('./src/assets/styles/main.scss') // './src/assets/**/*.scss'
+    .pipe(sass.sourcemaps.init())
+    .pipe(sass({sourceMap: true, outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({cascade: false}))
+    .pipe(csso())
+    .pipe(dest('out/css'));
+}
+
+exports.sass = sassCompile;
+
+// Compilação para typescript //
+function typescriptTranspile() {
+  const tsResult = src("src/**/*.ts") 
       .pipe(tsProject());
 
-  return tsResult.js.pipe(dest('release'));
+  return tsResult.js
+    .pipe(uglify())
+    .pipe(dest('out/release'));
 }
 
-// Compilação para ES6
-exports.babel = function() {
-  return src('src/*.js')
+exports.typescript = typescriptTranspile;
+
+// Compilação para ES6 //
+function babelTranspile() {
+  return src('src/js/*.js')
     .pipe(babel())
     .pipe(uglify())
-    .pipe(dest('out/'));
+    .pipe(dest('out/js'));
 }
 
-// TODO: juntar metodos em um compilador maior. ( https://gulpjs.com/docs/en/getting-started/creating-tasks )
-// TODO: montar um gulp watch ( https://gulpjs.com/docs/en/getting-started/watching-files )
+exports.babel = babelTranspile;
+
+// TODO: realizar compilação do gulp ( https://gulpjs.com/docs/en/getting-started/watching-files )
+exports.watch = function(){
+    watch('src/js/*.js', 'babel')
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import "babel-polyfill"; // https://babeljs.io/docs/en/babel-polyfill
 
 const name = "Guy Fieri";
-const place = "Flavortown";
+const place = "Flavortown"
 
 console.log(`Hello ${name}, ready for ${place}?`);
